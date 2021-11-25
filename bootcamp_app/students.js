51aa7ba26c097c9dd5f65c94ff0abfda6408c0cf
Natnael Tekletsadik <isaacnatnael@gmail.com>
2021-11-25 04:32:32 +0000

    impliment sql injection protection

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,24 +7,16 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-// pool.query(`
-// SELECT id, name, cohort_id
-// FROM students
-// LIMIT 5;
-// `)
-// .then(res => {
-//   console.log(res);
-// })
-// .catch(err => console.error('query error', err.stack));
 const cohort = process.argv[2];
 const limit = process.argv[3];
+const values = [`${cohort}%`, limit ]
 pool.query(`
 SELECT students.id, students.name, cohorts.name as cohort_name
 FROM students
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name  LIKE '${cohort}%'
-LIMIT ${limit};
-`)
+WHERE cohorts.name  LIKE $1
+LIMIT $2;
+`, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_name} cohort`);
